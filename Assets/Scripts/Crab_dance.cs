1534a494fcf5d8902312e8fa18f11e77bb7ea6a6
Tung19 <Khactung01092002@gmail.com>
2024-01-15 07:40:43 +0700

    pull

---
+++ Assets/Scripts/Crab_dance.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crab_dance : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public float jumpInterval = 2f;
+
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    private float lastJumpTime;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        lastJumpTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Time.time - lastJumpTime >= jumpInterval)
+        {
+            Jump();
+            lastJumpTime = Time.time;
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            // Áp dụng lực nhảy
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            // Không ở trên mặt đất sau khi nhảy
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
